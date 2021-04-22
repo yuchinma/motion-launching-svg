e0ee71b05377f9bc890e5dcebdd48ed40cc98468
yuchinma <63366430+yuchinma@users.noreply.github.com>
2021-04-22 16:00:22 -0400

    waves animated

--- dev/js/zoomIn.js
+++ dev/js/zoomIn.js
@@ -1,9 +1,12 @@
 import { gsap } from "gsap";
 
+// gsap.registerPlugin(CustomEase)
+// CustomEase.create("waves", "M0,0 C0,0 0.029,0.256 0.159,0.668 0.284,0.952 0.354,1.051 0.519,1.052 0.639,1.052 0.709,1.007 0.747,0.966 0.79,0.918 1,0.7 1,0.7 ")
+
 export let zoomTL = gsap.timeline();
 
 // gsap.set("#sand",{transformOrigin:"center"});
-gsap.set("#ocean",{transformOrigin:"left top"});
+gsap.set("#ocean",{transformOrigin:"right top"});
 // gsap.set("#sky",{transformOrigin:"center"});
 // gsap.set(".trees",{transformOrigin:"center"});
 
@@ -17,4 +20,9 @@ gsap.set("#ocean",{transformOrigin:"left top"});
             // .from("#trees-2",{duration:5, scale:10,ease: "power4.out",y:"+=420"},"zoom")
             // .from("#trees-1",{duration:4.75, scale:10,ease: "power4.out",y:"+=420"},"zoom")
 
-zoomTL.from("#ocean", {duration: 3, scale: 5});
+// zoomTL.from("#ocean", {duration: 3, scale: 5});
+
+zoomTL.to("#ocean", {duration: .5, scaleX: 1.3, scaleY: 1.5, ease: "power4.out"})
+        .to("#ocean", {duration: .5, scaleX: 1.1, scaleY: 1.1})
+        .to("#ocean", {duration: .5, scaleX: 1.3, scaleY: 1.5, ease: "power3.out"})
+        .to("#ocean", {duration: .5, scaleX: 1, scaleY: 1});
